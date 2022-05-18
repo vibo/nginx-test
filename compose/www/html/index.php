9d0f498adf6a6f1156b3a829084f2528d064ff59
Vibo <vibo92@hotmail.com>
2022-05-18 14:55:25 +0200

    wip

--- compose/www/html/index.php
+++ compose/www/html/index.php
@@ -5,9 +5,12 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
+  <link rel="stylesheet" href="./styles.css" type="text/css" />
 </head>
 <body>
-  <p>
+  <p>a
+    sd
+  <?php echo date("l jS F"); ?>
   <?php
     echo 'We are running PHP, version: ' . phpversion();
   ?>
